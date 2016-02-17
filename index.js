5cf6c698a427f3312bc4e68f588de13b5656f4d9
Jannis R <mail@jannisr.de>
2016-02-17 22:09:04 +0100

    better check for `Symbol.iterator` support

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
 module.exports = function (val) {
-	return (Symbol && Symbol.iterator
+	return (Symbol && 'iterator' in Symbol
 		&& val != null && 'function' === typeof val[Symbol.iterator]);
 };
